bcb00b14fef174b56e4b66375f650c3b23385b0d
Suchetan Saravanan <suchetan@clutterbot.com>
2025-05-03 12:06:11 +0530

    Files for deb build

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ COPY ./shell_scripts/basic_util.sh /root/
 RUN cd /root/ && chmod +x * && ./basic_util.sh && rm -rf basic_util.sh
 RUN apt-get update && apt-get upgrade -y && apt-get install sudo
 
-COPY shell_scripts/install_ros2_binary.sh /root/
-RUN cd /root/ && sudo chmod +x * && ./install_ros2_binary.sh
+COPY shell_scripts/install_ros2_deb.sh /root/
+RUN cd /root/ && sudo chmod +x * && ./install_ros2_deb.sh
 
 RUN sudo apt upgrade
 
@@ -16,5 +16,3 @@ RUN sudo apt update
 RUN sudo apt install -y python3-rosdep
 RUN sudo rosdep init
 RUN rosdep update
-COPY root_dir/ros2_kilted/ /root/ros2_kilted/
-RUN rosdep install --from-paths ~/ros2_kilted/ros2-linux/share --ignore-src -y --skip-keys "cyclonedds fastcdr fastrtps iceoryx_binding_c rmw_connextdds rti-connext-dds-7.3.0 urdfdom_headers fastdds"
--- shell_scripts/install_ros2_deb.sh
+++ shell_scripts/install_ros2_deb.sh
@@ -7,13 +7,17 @@ export LANG=en_US.UTF-8
 
 locale  # verify settings
 
-sudo apt install -y software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 
 sudo apt update && sudo apt install curl -y
 curl -o /tmp/ros2-testing-apt-source.deb https://ftp.osuosl.org/pub/ros/packages.ros.org/ros2-testing/ubuntu/pool/main/r/ros-apt-source/ros2-testing-apt-source_1.0.0~$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb
-sudo apt install -y /tmp/ros2-testing-apt-source.deb
+sudo apt install /tmp/ros2-testing-apt-source.deb -y
 
-sudo apt install -y tar bzip2 wget -y
+sudo apt update && sudo apt install ros-dev-tools -y
 
-sudo apt update && sudo apt install -y ros-dev-tools
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install ros-kilted-desktop -y
